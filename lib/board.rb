474e4f8f27bf220895932484751eb9803baf04a4
Monatric <nawpnawp8@gmail.com>
2024-10-18 20:01:53 +0800

    Add more files for the base of game

---
+++ lib/board.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# class for the Connect Four board
+class Board
+end
---
+++ lib/player.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# class for a player's instance
+class Person
+end
