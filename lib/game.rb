c0c9455685d874bae25260d2e20844df69845087
Monatric <nawpnawp8@gmail.com>
2024-10-18 19:06:37 +0800

    Add game class and a respective spec

---
+++ lib/game.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# class for starting and setting up the game Connect Four
+class Game
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/game'
+
+describe Game do
+  # describe '#select_column' do
+  #   context ''
+  # end
+end
