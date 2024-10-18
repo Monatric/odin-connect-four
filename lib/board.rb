1a66a8ab9b6b6229de1f79b112db4999e6befdff
Monatric <nawpnawp8@gmail.com>
2024-10-19 04:06:51 +0800

    Make a method that shows the board  and test it manually in main

--- lib/board.rb
+++ lib/board.rb
@@ -8,6 +8,27 @@ class Board
     @cells = cells
   end
 
+  # rubocop:disable Metrics/AbcSize
+  def show
+    puts <<-HEREDOC
+
+    | #{cells[[6, 1]]} | #{cells[[6, 2]]} | #{cells[[6, 3]]} | #{cells[[6, 4]]} | #{cells[[6, 5]]} | #{cells[[6, 6]]} | #{cells[[6, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[5, 1]]} | #{cells[[5, 2]]} | #{cells[[5, 3]]} | #{cells[[5, 4]]} | #{cells[[5, 5]]} | #{cells[[5, 6]]} | #{cells[[5, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[4, 1]]} | #{cells[[4, 2]]} | #{cells[[4, 3]]} | #{cells[[4, 4]]} | #{cells[[4, 5]]} | #{cells[[4, 6]]} | #{cells[[4, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[3, 1]]} | #{cells[[3, 2]]} | #{cells[[3, 3]]} | #{cells[[3, 4]]} | #{cells[[3, 5]]} | #{cells[[3, 6]]} | #{cells[[3, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[2, 1]]} | #{cells[[2, 2]]} | #{cells[[2, 3]]} | #{cells[[2, 4]]} | #{cells[[2, 5]]} | #{cells[[2, 6]]} | #{cells[[2, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[1, 1]]} | #{cells[[1, 2]]} | #{cells[[1, 3]]} | #{cells[[1, 4]]} | #{cells[[1, 5]]} | #{cells[[1, 6]]} | #{cells[[1, 7]]} |
+    +---+---+---+---+---+---+---+
+
+    HEREDOC
+  end
+  # rubocop:enable Metrics/AbcSize
+
   def create(rows = 6, cols = 7)
     board = {}
 
@@ -23,10 +44,7 @@ class Board
   def update(col, token)
     row = 6
     # subtract row to 1 to scan for the spot on which the token is supposed to land
-    while cells[[row - 1, col]] == ' '
-      row -= 1
-      break if row == 1
-    end
+    row -= 1 while cells[[row - 1, col]] == ' ' && row != 1
     cells[[row, col]] = token
   end
 end
---
+++ lib/main.rb
@@ -0,0 +1,7 @@
+require_relative 'board'
+require_relative 'game'
+require_relative 'player'
+
+# test
+# connect_four = Board.new
+# connect_four.show
