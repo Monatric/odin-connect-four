079770f32167df12b6f08ec45d5bb748690e989e
Monatric <nawpnawp8@gmail.com>
2024-10-18 22:42:41 +0800

    Create the 2D board for connect four using hash

--- lib/board.rb
+++ lib/board.rb
@@ -2,4 +2,21 @@
 
 # class for the Connect Four board
 class Board
+  attr_reader :cells
+
+  def initialize(cells = create)
+    @cells = cells
+  end
+
+  def create(rows = 6, cols = 7)
+    board = {}
+
+    (1..rows).each do |row|
+      (1..cols).each do |col|
+        board[[row, col]] = ' '
+      end
+    end
+
+    board
+  end
 end
