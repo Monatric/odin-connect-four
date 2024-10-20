03d0cb49921d6b3aa76f951ac36cb0576e0b6952
Monatric <nawpnawp8@gmail.com>
2024-10-20 23:25:52 +0800

    Test the switch of player 1 to player 2

--- lib/game.rb
+++ lib/game.rb
@@ -2,4 +2,22 @@
 
 # class for starting and setting up the game Connect Four
 class Game
+  attr_reader :board, :player1, :player2
+  attr_accessor :current_turn
+
+  def initialize(board = Board.new, player1 = Player.new('1', '♠'), player2 = Player.new('2', '♣'))
+    @board = board
+    @player1 = player1
+    @player2 = player2
+    @current_turn = @player1
+  end
+
+  def switch_turn
+    self.current_turn = (current_turn == player1 ? player2 : player1)
+    # if current_turn == player1
+    #   self.current_turn = player2
+    # else
+    #   self.current_turn = player1
+    # end
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,19 @@
 require_relative '../lib/game'
+require_relative '../lib/board'
 
 describe Game do
-  # describe '#select_column' do
-  #   context ''
-  # end
+  describe '#switch_turn' do
+    context 'when the current_turn is Player 1' do
+      let(:player1) { double('player1') }
+      let(:player2) { double('player2') }
+
+      let(:board) { instance_double(Board) }
+      subject(:game) { described_class.new(board, player1, player2) }
+
+      it 'changes to Player 2 on next turn' do
+        game.switch_turn
+        expect(game.current_turn).to be player2
+      end
+    end
+  end
 end
