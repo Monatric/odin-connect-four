949e3aac87842e4d8d04d1e8edb67cce7d96b36f
Monatric <nawpnawp8@gmail.com>
2024-10-20 23:04:58 +0800

    Add attributes for Person class

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
-# class for a player's instance
+# class for the connect four player
 class Person
+  attr_accessor :name, :token
+
+  def initialize(name, token)
+    @name = name
+    @token = token
+  end
 end
