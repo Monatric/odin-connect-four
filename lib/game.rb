bce9eb30d306d46c7013e5bf1a317feabe635a36
Monatric <nawpnawp8@gmail.com>
2024-10-21 18:49:29 +0800

    Add basic user interaction methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require_relative 'displayable'
+
 # frozen_string_literal: true
 
 # class for starting and setting up the game Connect Four
@@ -5,19 +7,31 @@ class Game
   attr_reader :board, :player1, :player2
   attr_accessor :current_turn
 
-  def initialize(board = Board.new, player1 = Player.new('1', '♠'), player2 = Player.new('2', '♣'))
+  include Displayable
+
+  def initialize(board = Board.new, player1 = Player.new('Player 1', '♠'), player2 = Player.new('Player 2', '♣'))
     @board = board
     @player1 = player1
     @player2 = player2
     @current_turn = @player1
   end
 
+  def drop_token(col, token)
+    board.update(col, token)
+    board.show
+    puts display_winner(current_turn) if board.game_over?(col)
+    switch_turn
+  end
+
   def switch_turn
     self.current_turn = (current_turn == player1 ? player2 : player1)
-    # if current_turn == player1
-    #   self.current_turn = player2
-    # else
-    #   self.current_turn = player1
-    # end
+  end
+
+  def conclusion
+    if board.game_over?
+      puts display_winner(current_turn)
+    else
+      puts display_tie
+    end
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,35 @@
 require_relative 'board'
 require_relative 'game'
 require_relative 'player'
+require_relative 'displayable'
 
-# test
-# connect_four = Board.new
-# connect_four.show
+game = Game.new
+
+def start(game)
+  puts 'Welcome to Connect Four!'
+  game.board.show
+  select_column(game)
+end
+
+def select_column(game)
+  until game.board.full?
+    puts "#{game.current_turn.name} turn."
+    print 'Select a column to drop the token: '
+
+    user_input = gets.chomp
+    user_input = verify_input(user_input, game)
+
+    game.drop_token(user_input, game.current_turn.token)
+    break if game.board.game_over?(user_input)
+  end
+end
+
+def verify_input(input, game)
+  until input.match?(/\A[1-7]\z/) && !game.board.full_column?(input.to_i)
+    print 'Invalid choice. Try again: '
+    input = gets.chomp
+  end
+  input.to_i
+end
+
+start(game)
