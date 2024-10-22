6e79c33bf3f851006b3f6d94a0f3b8ed285bff8c
Monatric <nawpnawp8@gmail.com>
2024-10-22 23:10:09 +0800

    Refactor: add a display_board instead of direct puts in board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,33 +1,20 @@
+require_relative 'displayable'
+
 # frozen_string_literal: true
 
 # class for the Connect Four board
 class Board
   attr_reader :cells
 
+  include Displayable
+
   def initialize(cells = create)
     @cells = cells
   end
 
-  # rubocop:disable Metrics/AbcSize
-  def show # rubocop:disable Metrics/MethodLength
-    puts <<-HEREDOC
-      1   2   3   4   5   6   7
-    | #{cells[[6, 1]]} | #{cells[[6, 2]]} | #{cells[[6, 3]]} | #{cells[[6, 4]]} | #{cells[[6, 5]]} | #{cells[[6, 6]]} | #{cells[[6, 7]]} |
-    +---+---+---+---+---+---+---+
-    | #{cells[[5, 1]]} | #{cells[[5, 2]]} | #{cells[[5, 3]]} | #{cells[[5, 4]]} | #{cells[[5, 5]]} | #{cells[[5, 6]]} | #{cells[[5, 7]]} |
-    +---+---+---+---+---+---+---+
-    | #{cells[[4, 1]]} | #{cells[[4, 2]]} | #{cells[[4, 3]]} | #{cells[[4, 4]]} | #{cells[[4, 5]]} | #{cells[[4, 6]]} | #{cells[[4, 7]]} |
-    +---+---+---+---+---+---+---+
-    | #{cells[[3, 1]]} | #{cells[[3, 2]]} | #{cells[[3, 3]]} | #{cells[[3, 4]]} | #{cells[[3, 5]]} | #{cells[[3, 6]]} | #{cells[[3, 7]]} |
-    +---+---+---+---+---+---+---+
-    | #{cells[[2, 1]]} | #{cells[[2, 2]]} | #{cells[[2, 3]]} | #{cells[[2, 4]]} | #{cells[[2, 5]]} | #{cells[[2, 6]]} | #{cells[[2, 7]]} |
-    +---+---+---+---+---+---+---+
-    | #{cells[[1, 1]]} | #{cells[[1, 2]]} | #{cells[[1, 3]]} | #{cells[[1, 4]]} | #{cells[[1, 5]]} | #{cells[[1, 6]]} | #{cells[[1, 7]]} |
-    +---+---+---+---+---+---+---+
-
-    HEREDOC
+  def show
+    puts display_board(cells)
   end
-  # rubocop:enable Metrics/AbcSize
 
   def create(rows = 6, cols = 7)
     board = {}
--- lib/displayable.rb
+++ lib/displayable.rb
@@ -7,4 +7,23 @@ module Displayable
   def display_tie
     "No player won. It's a tie."
   end
+
+  def display_board(cells) # rubocop:disable Metrics/AbcSize
+    <<-HEREDOC
+      1   2   3   4   5   6   7
+    | #{cells[[6, 1]]} | #{cells[[6, 2]]} | #{cells[[6, 3]]} | #{cells[[6, 4]]} | #{cells[[6, 5]]} | #{cells[[6, 6]]} | #{cells[[6, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[5, 1]]} | #{cells[[5, 2]]} | #{cells[[5, 3]]} | #{cells[[5, 4]]} | #{cells[[5, 5]]} | #{cells[[5, 6]]} | #{cells[[5, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[4, 1]]} | #{cells[[4, 2]]} | #{cells[[4, 3]]} | #{cells[[4, 4]]} | #{cells[[4, 5]]} | #{cells[[4, 6]]} | #{cells[[4, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[3, 1]]} | #{cells[[3, 2]]} | #{cells[[3, 3]]} | #{cells[[3, 4]]} | #{cells[[3, 5]]} | #{cells[[3, 6]]} | #{cells[[3, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[2, 1]]} | #{cells[[2, 2]]} | #{cells[[2, 3]]} | #{cells[[2, 4]]} | #{cells[[2, 5]]} | #{cells[[2, 6]]} | #{cells[[2, 7]]} |
+    +---+---+---+---+---+---+---+
+    | #{cells[[1, 1]]} | #{cells[[1, 2]]} | #{cells[[1, 3]]} | #{cells[[1, 4]]} | #{cells[[1, 5]]} | #{cells[[1, 6]]} | #{cells[[1, 7]]} |
+    +---+---+---+---+---+---+---+
+
+    HEREDOC
+  end
 end
